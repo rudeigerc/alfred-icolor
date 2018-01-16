5e411d788a064850f4a7748c7dcd6899f010ac98
rudeigerc <rudeigerc@gmail.com>
2018-01-17 00:39:41 +0800

    update test

--- test.js
+++ test.js
@@ -8,7 +8,8 @@ test(async t => {
 	t.deepEqual(result, [
 		{
 			title: 'UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)',
-			subtitle: '#FFFFFF'
+			subtitle: '#FFFFFF',
+			arg: 'UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)'
 		}
 	]);
 });
