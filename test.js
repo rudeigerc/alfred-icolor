3e105db3531dd4d58b27b44bfd5050ff7ca5c7d7
rudeigerc <rudeigerc@gmail.com>
2018-01-19 00:13:17 +0800

    update test samples

--- test.js
+++ test.js
@@ -1,7 +1,55 @@
 import test from 'ava';
 import alfyTest from 'alfy-test';
 
-test(async t => {
+test('hex3 valid', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('#000');
+
+	t.deepEqual(result, [
+		{
+			title: 'UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)',
+			subtitle: '#000',
+			arg: 'UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)'
+		}
+	]);
+});
+
+test('hex3 invalid', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('#FFG');
+
+	t.deepEqual(result, [
+		{
+			title: 'Invalid argument',
+		}
+	]);
+});
+
+test('hex4 valid', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('#FF00');
+
+	t.deepEqual(result, [
+		{
+			title: 'UIColor(red: 1.00, green: 0.00, blue: 0.00, alpha: 1.00)',
+			subtitle: '#FF00',
+			arg: 'UIColor(red: 1.00, green: 0.00, blue: 0.00, alpha: 1.00)'
+		}
+	]);
+});
+
+test('hex4 invalid', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('#FFFG');
+
+	t.deepEqual(result, [
+		{
+			title: 'Invalid argument',
+		}
+	]);
+});
+
+test('hex6 valid', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('#FFFFFF');
 
@@ -10,6 +58,63 @@ test(async t => {
 			title: 'UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)',
 			subtitle: '#FFFFFF',
 			arg: 'UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)'
+		}
+	]);
+});
+
+test('hex6 invalid', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('#FFFFFG');
+
+	t.deepEqual(result, [
+		{
+			title: 'Invalid argument',
+		}
+	]);
+});
+
+test('hex8 valid', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('#FFFF00FF');
+
+	t.deepEqual(result, [
+		{
+			title: 'UIColor(red: 1.00, green: 0.00, blue: 1.00, alpha: 1.00)',
+			subtitle: '#FFFF00FF',
+			arg: 'UIColor(red: 1.00, green: 0.00, blue: 1.00, alpha: 1.00)'
+		}
+	]);
+});
+
+test('hex8 invalid', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('#FFFFFFFG');
+
+	t.deepEqual(result, [
+		{
+			title: 'Invalid argument',
+		}
+	]);
+});
+
+test('format invalid', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('FFF');
+
+	t.deepEqual(result, [
+		{
+			title: 'Invalid argument',
+		}
+	]);
+});
+
+test('length invalid', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('#FFFFF');
+
+	t.deepEqual(result, [
+		{
+			title: 'Invalid argument',
 		}
 	]);
 });
